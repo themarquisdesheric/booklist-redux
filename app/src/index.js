af4042be56972dd930d672aaeee90eabde322615
Yuval Allweil <themarquisdesheric@gmail.com>
2018-01-13 11:11:14 -0800

    app now has persistent state via localStorage

--- app/src/index.js
+++ app/src/index.js
@@ -4,8 +4,20 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './App';
 import books from './reducers/books';
+import { loadState, saveState } from './localStorage';
 
-const store = createStore(books, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const persistedState = loadState();
+
+const store = createStore(
+  books, 
+  persistedState, 
+  // redux dev tools
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe( () => {
+  saveState(store.getState());
+});
 
 ReactDOM.render(
   <Provider store={store}>

68175628746b9f138019a0631e277a01c980eb49
Yuval Allweil <themarquisdesheric@gmail.com>
2018-01-13 20:38:57 -0800

    removeBook test passing

--- app/src/__test__/actions.test.js
+++ app/src/__test__/actions.test.js
@@ -1,14 +1,22 @@
 import c from '../constants/books';
-import { addBook } from '../actions';
+import { addBook, removeBook } from '../actions';
 
-describe('action creators', () => {
-  it('addBook', () => {
+describe('actions', () => {
+  it('should create an action to add a book', () => {
     const title = 'sapiens';
     const action = addBook(title);
 
     expect(action.type).toBe(c.ADD_BOOK);
     expect(action.payload.title).toEqual(title);
     expect(action.payload.id).toBeTruthy();
+    expect(typeof action.payload.id).toBe('string');
     expect(action.payload.read).toBeFalsy();
+  });
+  
+  it('should create an action to remove a book', () => {
+    const action = removeBook('someID');
+
+    expect(action.type).toBe(c.REMOVE_BOOK);
+    expect(action.payload).toBeTruthy();
   });
 });

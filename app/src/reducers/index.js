459c63c42847c537d2fc8f2c57aa7f492967480b
Yuval Allweil <themarquisdesheric@gmail.com>
2018-01-18 13:41:38 -0800

    added redux-thunk, removed console messages middleware

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers, applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
 import books from './books';
 import visibilityFilter from './visibilityFilter';
 import fetching from './fetching';
@@ -9,32 +10,8 @@ const appReducer = combineReducers({
   fetching
 });
 
-const consoleMessages = store => next => action => {
-  let result;
-
-  console.log(`
-  
-  current state: 
-    ${JSON.stringify(store.getState())}
-  
-  dispatching action: 
-    ${action.type}
-  `);
-  
-  result = next(action);
-  
-  console.log(`
-    action dispatched!
-
-    current state: 
-      ${JSON.stringify(store.getState())}
-  `);
-
-  return result;
-};
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default (initialState = {}) => {
-  return createStore(appReducer, initialState, composeEnhancers(applyMiddleware(consoleMessages)));
+  return createStore(appReducer, initialState, composeEnhancers(applyMiddleware(thunk)));
 };

f3e67db27a6a27d33e36608da9e6d3793a18e0ee
Yuval Allweil <themarquisdesheric@gmail.com>
2018-01-25 12:19:25 -0800

    updated README and minor styling

--- app/src/components/ui/Tabs.jsx
+++ app/src/components/ui/Tabs.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BookIcon from 'react-icons/lib/fa/book';
-import FilesIcon from 'react-icons/lib/fa/file-text';
+import CheckIcon from 'react-icons/lib/fa/check-square';
 import Tab from './Tab';
 
 const Tabs = ({ visibilityFilter }) => {
   const filters = ['Reading List', 'Finished Books'];
-  const icons = [BookIcon, FilesIcon];
+  const icons = [BookIcon, CheckIcon];
 
   return (
     <div className="tabs is-fullwidth is-toggle">

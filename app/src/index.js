b6a24f487d9c1449c5e27498464e0af5f21d45db
Yuval Allweil <themarquisdesheric@gmail.com>
2018-02-16 20:05:06 -0800

    switched to hash router for page reloads

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import throttle from 'lodash/throttle';
 import App from './components/ui/App';
 import storeFactory from './reducers';
